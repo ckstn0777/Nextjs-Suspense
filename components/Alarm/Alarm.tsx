11a28d9154c9833afb17089929c505b1f1474d16
Chansu Kim <ckstn0777@naver.com>
2023-02-11 16:47:28 +0900

    feat: prisma setting and create prisma schema

--- components/Alarm/Alarm.tsx
+++ components/Alarm/Alarm.tsx
@@ -13,9 +13,9 @@ export default function Alarm() {
               className="mx-2 h-6 w-6 stroke-current"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               ></path>
             </svg>
